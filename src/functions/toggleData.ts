6549bc0aac370f61fd4b53aa7224bd4fb3c5fc60
Mustafa 'pxsty' KÖK <75846829+pxsty0@users.noreply.github.com>
2025-04-06 11:51:58 +0300

    added toggleData function

---
+++ src/functions/toggleData.ts
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+import { Config } from "../types/config";
+
+export default async function toggleData(
+  key: string,
+  config: Config
+): Promise<void> {
+  try {
+    const response = await fetch(`${config.baseURL}/database/toggleData`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: config.apiKey,
+      },
+      body: JSON.stringify({ key }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`API error: ${data.message}`);
+    }
+
+    return;
+  } catch (err: any) {
+    throw new Error(`Error sending request: ${err.toString()}`);
+  }
+}
